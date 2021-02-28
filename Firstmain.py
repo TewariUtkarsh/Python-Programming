0c82333b431e793ded4d8f95fc590f3204591869
UT <tewariutkarsh@outlook.com>
2021-02-28 18:27:08 +0530

    Firstmain.py

---
+++ Firstmain.py
@@ -0,0 +1,20 @@
+print("Hello World")    # Print Statement to Display Output 
+
+# Storing values in variables
+a = 10                  
+b = 50                  
+
+# Printing values stored in the variables
+print("a =",a)          
+print("b =",b)
+
+# Perforing operations on variables and swapping variables:
+a+=b
+b = a - b
+a-=b
+print("\nAfter swapping \na =",a)
+print("b =",b)
+
+# > Comparison Operator: 
+c = a>b
+print(c)
