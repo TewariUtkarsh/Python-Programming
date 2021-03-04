d8bf600f1541162c12be16c65bb517f7c5837b52
UT <tewariutkarsh@outlook.com>
2021-03-05 00:28:32 +0530

    Tuples in Python3

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+# myCars = ("Toyota","Mercedes","BMW","Audi","BMW")     #Tuple declared 
+# print("\nTuple: ",myCars)
+# # Tuple has only 2 built in functions:
+# # 1.count()
+# print("\nNumber of times BMW is present in the Tuple: ",myCars.count("BMW"))
+# print("Number of times Audi is present in the Tuple: ",myCars.count('Audi'))
+
+# # 2.index()
+# print("\nToyota is present at index ",myCars.index("Toyota"))
+
+# print(f"\nTuple: {myCars}")
+
+sq = lambda x : x * x
+sq(4)
