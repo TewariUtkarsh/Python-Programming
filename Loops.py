84566e451cd97f5a97df49ee84d88ea0ac809e36
UT <tewariutkarsh@outlook.com>
2021-03-08 22:20:05 +0530

    Loops in Pyhton 3

---
+++ Loops.py
@@ -0,0 +1,19 @@
+# FOR LOOP:
+list1 = ["Apple", 6, "Banana", 99, "Mango", 95.5, 100, -88, 0, 2, -50000, 10000]
+
+print("Elements Eligible: ")
+
+for item in list1:
+    if str(item).isnumeric() and item>6.0 :
+        print(item,end = " ")
+
+# WHILE LOOP:
+while(True):
+    a = int(input("Enter a number: "))
+    if(a>100):
+        print("\nCongratulations Number is greater than 100\nThank You for using the program")
+        exit(0)
+    print("\nNumber less than 100\n")
+    continue
+    
+    
