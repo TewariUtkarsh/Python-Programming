482908fa8b7c638f27184f5fa9f8cca938afa54c
UT <tewariutkarsh@outlook.com>
2021-03-10 23:16:42 +0530

    Tuples in Python 3

--- Tuple.py
+++ Tuple.py
@@ -1,14 +1,13 @@
-# myCars = ("Toyota","Mercedes","BMW","Audi","BMW")     #Tuple declared 
-# print("\nTuple: ",myCars)
-# # Tuple has only 2 built in functions:
-# # 1.count()
-# print("\nNumber of times BMW is present in the Tuple: ",myCars.count("BMW"))
-# print("Number of times Audi is present in the Tuple: ",myCars.count('Audi'))
+myCars = ("Toyota","Mercedes","BMW","Audi","BMW")     #Tuple declared 
+print("\nTuple: ",myCars)
 
-# # 2.index()
-# print("\nToyota is present at index ",myCars.index("Toyota"))
+# Tuple has only 2 built in functions:
 
-# print(f"\nTuple: {myCars}")
+# 1.count()- To count the number of Element ,that is passed as the Parameter, in the Tuple
+print("\nNumber of times BMW is present in the Tuple: ",myCars.count("BMW")) 
+print("Number of times Audi is present in the Tuple: ",myCars.count('Audi'))
 
-sq = lambda x : x * x
-sq(4)
+# 2.index()- Returns the Index of the Element that is passed as an Arguement
+print("\nToyota is present at index ",myCars.index("Toyota"))
+
+print(f"\nTuple: {myCars}")
