4f51c98270c43e99dbc06c193b554a198183de24
UT <tewariutkarsh@outlook.com>
2021-03-10 23:16:04 +0530

    Lists in Python 3

--- Lists.py
+++ Lists.py
@@ -1,22 +1,22 @@
 print("\nWelcome to Program demonstrating about Lists in Python: ")
 
 x = input("\nEnter the elements separted by commas:\n")
-lst = list(x.split(","))
+lst = list(x.split(","))    # Spliting string input, x, thus converting a List 
 
 print("\nList entered: ",lst)
 
 p = int(input("\nEnter the Index for Insertion: "))
 x = input("Enter the element to be inserted: ")
-lst.insert(p,x)
+lst.insert(p,x)     # Inserting element x at index p
 
 print("\nList after Insertion: ",lst)
 
 p = int(input("\nEnter the Index at which you have to perform removal: "))
-lst.pop(p-1)
+lst.pop(p-1)        # Deleting the element at the last index
 
 print("\nList after Removal: ",lst)
 
-lst.sort()
+lst.sort()          # Sorting the List
 print(f"\nSorted List: {lst}")
 
 # print(f"\nElement: {lst[8]}")
