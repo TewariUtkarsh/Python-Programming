1bea5d3907ddd77840ffa6b24e3751d80b0fc197
UT <tewariutkarsh@outlook.com>
2021-03-15 23:37:45 +0530

    Lambda Functions in Python 3

---
+++ Lambda Function.py
@@ -0,0 +1,30 @@
+# # add = lambda x,y: x+y
+
+# # def add(x,y):
+# #     print("Sum:",x+y)
+
+
+# # x = 100
+# # y = 100
+# # z = add(x,y)
+# # add(x,y)
+# # print("Sum:",z)
+
+# list1 = [ 101, 88, -111, 90, -6, 6 ]
+
+# list1.sort()
+# print(list1)
+
+# def s(list2):
+#     return list2[1]
+
+# list2 = [ [1,336], [1,-88,], [1,90], [1,101], [1,89]]
+# list2.sort(key= s)
+# print(list2)
+
+# import playsound 
+# playsound('01 - Marcin Przybylowicz - V.mp3')
+
+add = lambda x,y: x+y
+a = add(2,3)
+print(type(a),a)
